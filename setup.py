c90f18bb4a00010e61d5f7939d48dd169b3bc8dc
PunchyArchy <ksmdrmvstchny@gmail.com>
2021-08-19 10:56:21 +0500

    requirements.txt upd

--- setup.py
+++ setup.py
@@ -11,5 +11,12 @@ setup(
     license='MIT',
     author='Arthur',
     author_email='ksmdrmvstchny@gmail.com',
-    description='GCQDK - SDK для работы с API GCore Compound.'
+    description='GCQDK - SDK для работы с API GCore Compound.',
+    docs_extras = [
+        'Sphinx >= 3.0.0',  # Force RTD to use >= 3.0.0
+        'docutils',
+        'pylons-sphinx-themes >= 1.0.8',  # Ethical Ads
+        'pylons_sphinx_latesturl',
+        'repoze.sphinx.autointerface',
+        'sphinxcontrib-autoprogram']
 )
